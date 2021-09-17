196dd08d0cf7c4981ad05ab566eff5bf40398194
van sy <nguyenvansy031999@gmail.com>
2021-09-17 22:31:43 +0700

    update

--- src/urlConfig.js
+++ src/urlConfig.js
@@ -1,5 +1,4 @@
-const baseUrl =
-    location.hostname === "localhost" ? "http://localhost:4000" : "https://freshmarket-rest-server.herokuapp.com";
+const baseUrl = "https://freshmarket-rest-server.herokuapp.com";
 
 export const api = `${baseUrl}/api`;
 
